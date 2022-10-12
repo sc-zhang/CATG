4dc4a04de195248572690ed4b829f5cbf0a0efc4
sc-zhang <zsc-zhang@foxmail.com>
2022-10-12 21:13:18 +0800

    Fix Qt warning

--- MCAAG.py
+++ MCAAG.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 from PySide2.QtWidgets import QApplication
 from PySide2.QtGui import QIcon
+from PySide2.QtCore import QCoreApplication, Qt
 from qt_material import apply_stylesheet
 from coll_asm_adj_gui.windows.assembly_adjuster_main import AssemblyAdjusterMain
 from coll_asm_adj_gui.io.resources_loader import resource_path
 
 
 if __name__ == "__main__":
+    QCoreApplication.setAttribute(Qt.AA_ShareOpenGLContexts)
     app = QApplication([])
     icon_path = resource_path("coll_asm_adj_gui/ui/MCAAG.png")
     app.setWindowIcon(QIcon(icon_path))
