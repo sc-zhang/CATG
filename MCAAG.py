18fcbfc619edd872b6871f230e9665f6c631cb61
zero <3406985348@qq.com>
2022-10-14 04:39:34 +0800

    Move icons to icons folder and update README.md

--- MCAAG.py
+++ MCAAG.py
@@ -11,7 +11,7 @@ from coll_asm_adj_gui.io.resources_loader import resource_path
 if __name__ == "__main__":
     QCoreApplication.setAttribute(Qt.AA_ShareOpenGLContexts)
     app = QApplication([])
-    icon_path = resource_path("coll_asm_adj_gui/ui/MCAAG.png")
+    icon_path = resource_path("coll_asm_adj_gui/icons/MCAAG.png")
     app.setWindowIcon(QIcon(icon_path))
     main_window = AssemblyAdjusterMain()
     apply_stylesheet(app, theme="dark_teal.xml")
